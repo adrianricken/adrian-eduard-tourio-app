f4248d9fe4631b7af6815eb430942cdf988ad886
Adrian Ricken <adrianricken@gmail.com>
2024-09-20 17:28:50 +0200

    task Create Form  solution

--- pages/api/places/index.js
+++ pages/api/places/index.js
@@ -9,6 +9,12 @@ export default async function handler(request, response) {
       const places = await Place.find();
       return response.status(200).json(places);
     }
+    if (request.method === "POST") {
+      Place.create(request.body);
+      return response
+        .status(200)
+        .json({ success: true, status: "Place added successfully" });
+    }
   } catch (error) {
     console.log(error);
   }
